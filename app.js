0d88fc4f558d433bddc24170f881847c269a62b4
Joshua Rudio <joshuarudio@gmail.com>
2018-03-27 18:16:58 -0700

    Complete building server and reorganize files

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-const = process.env.port || 8000;
-app.listen(8000);
+const port = process.env.port || 8000;
+
+app.listen(port);
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/menu', (req, res) => {
+  res.render('pages/menu');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('pages/contact');
+});
